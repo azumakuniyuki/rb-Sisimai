ef2cd9f695e55cda2b623c70445fa63fe093539f
azumakuniyuki <azumakuniyuki@gmail.com>
2024-11-25 11:08:46 +0900

    Remove "hardbounce" accessor from Sisimai::Lhost #313

--- lib/sisimai/lhost.rb
+++ lib/sisimai/lhost.rb
@@ -22,7 +22,6 @@ module Sisimai
           'replycode'    => nil,  # SMTP Reply code
           'diagnosis'    => nil,  # The value of Diagnostic-Code header
           'recipient'    => nil,  # The value of Final-Recipient header
-          'hardbounce'   => nil,  # Hard bounce or not
           'feedbacktype' => nil,  # Feedback Type
         }
       end
