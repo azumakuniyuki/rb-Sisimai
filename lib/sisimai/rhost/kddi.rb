c0bc40c6f7abe54d6e42315b3b235aaefa724bdf
azumakuniyuki <azumakuniyuki@gmail.com>
2024-12-16 09:35:34 +0900

    Check the value of diagnosticcode strictly

--- lib/sisimai/rhost/kddi.rb
+++ lib/sisimai/rhost/kddi.rb
@@ -15,7 +15,7 @@ module Sisimai
         # @return   [String]                The bounce reason for au.com or ezweb.ne.jp
         # @since v4.22.6
         def find(argvs)
-          return argvs['reason'] unless argvs['reason'].empty?
+          return "" unless argvs["diagnosticcode"].empty?
           issuedcode = argvs['diagnosticcode'].downcase
           reasontext = ''
 
