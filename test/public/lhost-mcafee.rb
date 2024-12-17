fdf0ee72bc5a2a8f084bc4b4ad33ae1afe0209aa
azumakuniyuki <azumakuniyuki@gmail.com>
2024-12-17 17:38:05 +0900

    Update the results for McAfee

--- test/public/lhost-mcafee.rb
+++ test/public/lhost-mcafee.rb
@@ -2,11 +2,11 @@ module LhostEngineTest::Public
   module McAfee
     IsExpected = {
       # INDEX => [['D.S.N.', 'replycode', 'REASON', 'hardbounce'], [...]]
-      '01' => [['5.0.911', '550', 'userunknown',     true]],
+      '01' => [['5.0.910', '550', 'filtered',       false]],
       '02' => [['5.1.1',   '550', 'userunknown',     true]],
       '03' => [['5.1.1',   '550', 'userunknown',     true]],
-      '04' => [['5.0.911', '550', 'userunknown',     true]],
-      '05' => [['5.0.911', '550', 'userunknown',     true]],
+      '04' => [['5.0.910', '550', 'filtered',       false]],
+      '05' => [['5.0.910', '550', 'filtered',       false]],
     }
   end
 end
