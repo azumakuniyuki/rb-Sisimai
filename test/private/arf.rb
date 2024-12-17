8141736186f1c40ca3a7bca2f119c2e31fede124
azumakuniyuki <azumakuniyuki@gmail.com>
2024-12-17 17:31:39 +0900

    Some sample emails which does not have "Content-Type: message/feedback-report" field https://github.com/sisimai/set-of-emails/issues/25

--- test/private/arf.rb
+++ test/private/arf.rb
@@ -9,16 +9,16 @@ module LhostEngineTest::Private
       '01005' => [['', '', 'feedback', false, 'abuse'       ]],
       '01006' => [['', '', 'feedback', false, 'abuse'       ]],
       '01007' => [['', '', 'feedback', false, 'abuse'       ]],
-      '01008' => [['', '', 'feedback', false, 'abuse'       ]],
+#     '01008' => [['', '', 'feedback', false, 'abuse'       ]],
       '01009' => [['', '', 'feedback', false, 'abuse'       ]],
       '01010' => [['', '', 'feedback', false, 'abuse'       ]],
       '01011' => [['', '', 'feedback', false, 'opt-out'     ]],
       '01012' => [['', '', 'feedback', false, 'abuse'       ]],
-      '01013' => [['', '', 'feedback', false, 'abuse'       ]],
-      '01014' => [['', '', 'feedback', false, 'abuse'       ]],
+#     '01013' => [['', '', 'feedback', false, 'abuse'       ]],
+#     '01014' => [['', '', 'feedback', false, 'abuse'       ]],
       '01015' => [['', '', 'feedback', false, 'abuse'       ]],
       '01016' => [['', '', 'feedback', false, 'auth-failure']],
-      '01017' => [['', '', 'feedback', false, 'abuse'       ]],
+#     '01017' => [['', '', 'feedback', false, 'abuse'       ]],
     }
   end
 end
