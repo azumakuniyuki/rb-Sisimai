860660f2d8bb49e1bd56730fb3e1d4d7ec55741b
azumakuniyuki <azumakuniyuki@gmail.com>
2024-12-17 17:26:19 +0900

    rhost-franceptt-10,11 is "blocked" reason

--- test/public/rhost-franceptt.rb
+++ test/public/rhost-franceptt.rb
@@ -10,8 +10,8 @@ module RhostEngineTest::Public
       '06' => [['4.0.0',   '',    'blocked',         false]],
       '07' => [['4.0.0',   '421', 'blocked',         false]],
       '08' => [['4.2.0',   '421', 'systemerror',     false]],
-      '10' => [['5.5.0',   '550', 'undefined',       false]],
-      '11' => [['4.2.1',   '421', 'undefined',       false]],
+      '10' => [['5.5.0',   '550', 'blocked',         false]],
+      '11' => [['4.2.1',   '421', 'blocked',         false]],
       '12' => [['5.7.1',   '554', 'policyviolation', false]],
     }
   end
