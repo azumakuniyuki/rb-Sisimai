f139edf45b1f84d724ebf6043a45700af1d4254c
azumakuniyuki <azumakuniyuki@gmail.com>
2024-12-17 17:27:54 +0900

    rhost-facebook-03 is "userunknown" reason

--- test/public/rhost-facebook.rb
+++ test/public/rhost-facebook.rb
@@ -2,7 +2,7 @@ module RhostEngineTest::Public
   module Facebook
     IsExpected = {
       # INDEX => [['D.S.N.', 'replycode', 'REASON', 'hardbounce'], [...]]
-      '03' => [['5.1.1',   '550', 'filtered',        false]],
+      '03' => [['5.1.1',   '550', 'userunknown',     true]],
       '04' => [['5.1.1',   '550', 'userunknown',     true]],
     }
   end
