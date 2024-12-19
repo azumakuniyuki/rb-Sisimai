f700fbae7ea0ec7c99517f6a5d544d6b043f1312
azumakuniyuki <azumakuniyuki@gmail.com>
2024-12-19 22:15:35 +0900

    Disable 2 private samples for ApacheJames (there is no valid email address in From:)

--- test/private/lhost-apachejames.rb
+++ test/private/lhost-apachejames.rb
@@ -5,8 +5,8 @@ module LhostEngineTest::Private
       '01001' => [['5.0.910', '550', 'filtered',        false]],
       '01002' => [['5.0.910', '550', 'filtered',        false]],
       '01003' => [['5.0.910', '550', 'filtered',        false]],
-      '01004' => [['5.0.901', '',    'onhold',          false]],
-      '01005' => [['5.0.901', '',    'onhold',          false]],
+#     '01004' => [['5.0.901', '',    'onhold',          false]],
+#     '01005' => [['5.0.901', '',    'onhold',          false]],
     }
   end
 end
