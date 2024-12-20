830eece643e54d26231e78782a9dc5f572f661e1
azumakuniyuki <azumakuniyuki@gmail.com>
2024-12-21 05:08:35 +0900

    Change the bounce reason from "SecurityError" to "FailedSTARTTLS" in Sisimai::Rhost::Microsoft #322

--- lib/sisimai/rhost/microsoft.rb
+++ lib/sisimai/rhost/microsoft.rb
@@ -10,42 +10,6 @@ module Sisimai
     module Microsoft
       class << self
         MessagesOf = {
-          'failedstarttls' => [
-            # - DNSSEC checks have passed, yet upon establishing the connection the destination
-            #   mail server provides a certificate that is expired.
-            # - A valid X.509 certificate that isn't expired must be presented. X.509 certificates
-            #   must be renewed after their expiration, commonly annually.
-            ['5.7.51', 0, 0, 'restrictdomainstoipaddresses or restrictdomainstocertificate'],
-            ['5.7.322', 0, 0, "certificate-expired: destination mail server's certificate is expired"],
-
-            # - Records are DNSSEC authentic, but one or multiple of these scenarios occurred:
-            #   - The destination mail server's certificate doesn't match with what is expected per
-            #     the authentic TLSA record.
-            #   - Authentic TLSA record is misconfigured.
-            #   - Destination domain is being attacked.
-            #   - Any other DANE failure.
-            # - This message usually indicates an issue on the destination email server. Check the
-            #   validity of recipient address and determine if the destination server is configured
-            #   correctly to receive messages. 
-            # - For more information about DANE, see: https://datatracker.ietf.org/doc/html/rfc7671
-            ['4.7.323', 0, 0, 'tlsa-invalid: The domain failed dane validation'],
-            ['5.7.323', 0, 0, 'tlsa-invalid: The domain failed dane validation'],
-
-            # - The destination domain indicated it was DNSSEC-authentic, but Exchange Online was 
-            #   not able to verify it as DNSSEC-authentic.
-            ['4.7.324', 0, 0, 'dnssec-invalid: destination domain returned invalid dnssec records'],
-            ['5.7.324', 0, 0, 'dnssec-invalid: destination domain returned invalid dnssec records'],
-
-            # - This happens when the presented certificate identities (CN and SAN) of a destina-
-            #   tion SMTP target host don't match any of the domains or MX host.
-            # - This message usually indicates an issue on the destination email server. Check the
-            #   validity of recipient address and determine if the destination server is configured
-            #   correctly to receive messages. For more information, see How SMTP DNS-based Authen-
-            #   tication of Named Entities (DANE) works to secure email communications.
-            ['4.7.325', 0, 0, 'certificate-host-mismatch: remote certificate must have a common name or subject alternative name matching the hostname (dane)'],
-            ['5.7.325', 0, 0, 'certificate-host-mismatch: remote certificate must have a common name or subject alternative name matching the hostname (dane)'],
-
-          ],
           'authfailure' => [
             # - Access denied, a message sent over IPv6 [2a01:111:f200:2004::240] must pass either
             #   SPF or DKIM validation, this message is not signed
@@ -217,6 +181,53 @@ module Sisimai
             #   tion server never responded or the sent message generated an NDR error and that NDR
             #   couldn't be delivered to the original sender.
             ['5.4.300', 0, 0, 'message expired'],
+          ],
+          'failedstarttls' => [
+            # Exchange Online ---------------------------------------------------------------------
+            # - DNSSEC checks have passed, yet upon connection, destination mail server doesn't re-
+            #   spond to the STARTTLS command. The destination server responds to the STARTTLS com-
+            #   mand, but the TLS handshake fails.
+            # - This message usually indicates an issue on the destination email server. Check the
+            #   validity of the recipient address. Determine if the destination server is configur-
+            #   ed correctly to receive the messages.
+            ['4.4.317', 0, 0, 'starttls is required to send mail'],
+            ['5.4.317', 0, 0, 'starttls is required to send mail'],
+
+            # - DNSSEC checks have passed, yet upon establishing the connection the destination
+            #   mail server provides a certificate that is expired.
+            # - A valid X.509 certificate that isn't expired must be presented. X.509 certificates
+            #   must be renewed after their expiration, commonly annually.
+            ['5.7.51',  0, 0, 'restrictdomainstoipaddresses or restrictdomainstocertificate'],
+            ['4.7.321', 0, 0, 'starttls-not-supported: destination mail server must support tls to receive mail'],
+            ['5.7.321', 0, 0, 'starttls-not-supported: destination mail server must support tls to receive mail'],
+            ['5.7.322', 0, 0, "certificate-expired: destination mail server's certificate is expired"],
+
+            # - Records are DNSSEC authentic, but one or multiple of these scenarios occurred:
+            #   - The destination mail server's certificate doesn't match with what is expected per
+            #     the authentic TLSA record.
+            #   - Authentic TLSA record is misconfigured.
+            #   - Destination domain is being attacked.
+            #   - Any other DANE failure.
+            # - This message usually indicates an issue on the destination email server. Check the
+            #   validity of recipient address and determine if the destination server is configured
+            #   correctly to receive messages. 
+            # - For more information about DANE, see: https://datatracker.ietf.org/doc/html/rfc7671
+            ['4.7.323', 0, 0, 'tlsa-invalid: The domain failed dane validation'],
+            ['5.7.323', 0, 0, 'tlsa-invalid: The domain failed dane validation'],
+
+            # - The destination domain indicated it was DNSSEC-authentic, but Exchange Online was 
+            #   not able to verify it as DNSSEC-authentic.
+            ['4.7.324', 0, 0, 'dnssec-invalid: destination domain returned invalid dnssec records'],
+            ['5.7.324', 0, 0, 'dnssec-invalid: destination domain returned invalid dnssec records'],
+
+            # - This happens when the presented certificate identities (CN and SAN) of a destina-
+            #   tion SMTP target host don't match any of the domains or MX host.
+            # - This message usually indicates an issue on the destination email server. Check the
+            #   validity of recipient address and determine if the destination server is configured
+            #   correctly to receive messages. For more information, see How SMTP DNS-based Authen-
+            #   tication of Named Entities (DANE) works to secure email communications.
+            ['4.7.325', 0, 0, 'certificate-host-mismatch: remote certificate must have a common name or subject alternative name matching the hostname (dane)'],
+            ['5.7.325', 0, 0, 'certificate-host-mismatch: remote certificate must have a common name or subject alternative name matching the hostname (dane)'],
           ],
           'mailboxfull' => [
             # Exchange Server 2019 ----------------------------------------------------------------
@@ -450,19 +461,6 @@ module Sisimai
             #   method . Verify the remote IP address ranges on any custom Receive connectors.
             ['5.7.3', 0, 0, 'cannot achieve exchange server authentication'],
             ['5.7.3', 0, 0, 'not authorized'],
-
-            # Exchange Online ---------------------------------------------------------------------
-            # - DNSSEC checks have passed, yet upon connection, destination mail server doesn't re-
-            #   spond to the STARTTLS command. The destination server responds to the STARTTLS com-
-            #   mand, but the TLS handshake fails.
-            # - This message usually indicates an issue on the destination email server. Check the
-            #   validity of the recipient address. Determine if the destination server is configur-
-            #   ed correctly to receive the messages.
-            ['4.4.317', 0, 0, 'starttls is required to send mail'],
-            ['5.4.317', 0, 0, 'starttls is required to send mail'],
-
-            ['4.7.321', 0, 0, 'starttls-not-supported: destination mail server must support tls to receive mail'],
-            ['5.7.321', 0, 0, 'starttls-not-supported: destination mail server must support tls to receive mail'],
 
             # - The sending email system didn't authenticate with the receiving email system. The
             #   receiving email system requires authentication before message submission.
