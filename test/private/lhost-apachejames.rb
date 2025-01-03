db578703a08417626e2f806571f0bda1c01327d0
azumakuniyuki <azumakuniyuki@gmail.com>
2025-01-04 08:05:06 +0900

    Enable 2 private samples: 1004, 1005 in lhost-apachejames

--- test/private/lhost-apachejames.rb
+++ test/private/lhost-apachejames.rb
@@ -5,8 +5,8 @@ module LhostEngineTest::Private
       '1001'  => [['5.0.910', '550', 'filtered',        false]],
       '1002'  => [['5.0.910', '550', 'filtered',        false]],
       '1003'  => [['5.0.910', '550', 'filtered',        false]],
-#     '1004'  => [['5.0.901', '',    'onhold',          false]],
-#     '1005'  => [['5.0.901', '',    'onhold',          false]],
+      '1004'  => [['5.0.901', '',    'onhold',          false]],
+      '1005'  => [['5.0.901', '',    'onhold',          false]],
     }
   end
 end
